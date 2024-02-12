8c1498523658c995a4027f013740ec64d32919b7
Mirko Bozzetto <mirkouch@gmail.com>
2024-02-12 15:08:54 +0100

    Mise à jour du style CSS et ajout de contenu à l'application

--- 02weather/src/App.jsx
+++ 02weather/src/App.jsx
@@ -1,12 +1,20 @@
-function App() {
- 
-  return (
+import loader from "./assets/loader.svg";
+import "./App.css";
 
-      <main>
-      
-      </main>
-  
+const App = () => {
+  return (
+    <main>
+      <div className="loader-container">
+        <img src={loader} alt="loader" />
+      </div>
+      <p className="city-name">Bxl</p>
+      <p className="country-name">Belgique</p>
+      <p className="temperature">30°</p>
+      <div className="info-icon-container">
+        <img src="../public/icons/01d.svg" alt="weather icon" />
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
